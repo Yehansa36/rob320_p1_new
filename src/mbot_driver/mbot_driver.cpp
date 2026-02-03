78bd174097749ea71ec7f2c4a512a4b3508ff639
Yehansa <insarid7@gmail.com>
2026-02-03 16:56:03 +0000

    Final mbot driver file

--- src/mbot_driver/mbot_driver.cpp
+++ src/mbot_driver/mbot_driver.cpp
@@ -1,12 +1,9 @@
 #include "mbot_driver/mbot_driver.hpp"
-//#include <cstring>
-//#include <vector>
-//#include "rix/util/time.hpp"
+
 
 
 using namespace rix::ipc;
 using namespace rix::msg;
-//using namespace rix::util;
 
 
 MBotDriver::MBotDriver(std::unique_ptr<interfaces::IO> input, std::unique_ptr<MBotBase> mbot)
@@ -34,12 +31,13 @@ void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {
    //read next 4 bytes
    size_t offset = 0;
    uint8_t size_buf[sizeof(uint32_t)];
-
-
    ssize_t n = input->read(size_buf, sizeof(uint32_t));
+
+   //if EOF or read error, exit the loop
    if (n <= 0) {
-       continue;
+       break;
    }
+
    //deserialize msg size
    offset = 0;
    if (!msg_size.deserialize(size_buf, sizeof(uint32_t), offset)) {
@@ -51,8 +49,6 @@ void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {
    //read serialized Twist2DStamped
    std::vector<uint8_t> msg_buf(payload_size);
    size_t bytes_read = 0;
-
-
    while (bytes_read < payload_size) {
        ssize_t r = input->read( msg_buf.data() + bytes_read, payload_size - bytes_read);
 
@@ -65,6 +61,7 @@ void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {
    if (bytes_read != payload_size) {
        continue;
    }
+
    //deserialize into Twist2Dstamped
    geometry::Twist2DStamped twist_msg;
    offset = 0;
@@ -77,10 +74,13 @@ void MBotDriver::spin(std::unique_ptr<interfaces::Notification> notif) {
    }
 
    geometry::Twist2DStamped stop_msg;
-
    stop_msg.twist.vx = 0.0;
    stop_msg.twist.vy = 0.0;
    stop_msg.twist.wz = 0.0;
+
+   stop_msg.header.seq = 0;
+   stop_msg.header.frame_id = "mbot";
+   stop_msg.header.stamp.sec = rix::util::Time::now().to_milliseconds();
 
    mbot->drive(stop_msg);
 }
